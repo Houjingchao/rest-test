2810f849893f8c0eec016699e8af24e94f1df677
jaf <code_writer@163.com>
2016-08-25 21:03:13 +0800

    UPDATE

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -1,12 +1,19 @@
 package hello;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 /**
  * Created by jaf on 16/8/25.
  */
-public class Application {
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
+public class Application{
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 }
+
